46b0eabedbbaa3e1cdd4365846bb981b65cec722
Zakir Khan <39201626+threatjpn@users.noreply.github.com>
2026-01-13 14:36:53 +0000

    Clarify grid setter block placement

--- THREATEXEC/Private/BezierDebugActor.cpp
+++ THREATEXEC/Private/BezierDebugActor.cpp
@@ -79,14 +79,31 @@ void ABezierDebugActor::ApplyCurveSetDebug(ABezierCurveSetActor* CurveSet) const
 	CurveSet->UI_SetShowControlPointsForAll(bShowControlPoints);
 	CurveSet->UI_SetShowStripForAll(bShowStrip);
 	CurveSet->UI_SetShowCubeStripForAll(bUseCubeStrip);
-	CurveSet->UI_SetControlPointSizeForAll(ControlPointSize);
-	CurveSet->UI_SetStripSizeForAll(StripWidth, StripThickness);
+	if (bOverrideVisualSizes)
+	{
+		CurveSet->UI_SetControlPointSizeForAll(ControlPointSize);
+		CurveSet->UI_SetStripSizeForAll(StripWidth, StripThickness);
+	}
 	CurveSet->UI_SetControlPointColorsForAll(ControlPointNormal, ControlPointHover, ControlPointSelected);
 	CurveSet->UI_SetSnapToGridForAll(bSnapToGrid);
 	CurveSet->UI_SetShowGridForAll(bShowGrid);
 	CurveSet->UI_SetGridSizeForAll(GridSizeCm);
+	CurveSet->UI_SetGridExtentForAll(GridExtentCm);
+	CurveSet->UI_SetGridOriginWorldForAll(GridOriginWorld);
+	CurveSet->UI_SetGridColorForAll(GridColor);
+	CurveSet->UI_SetGridBaseAlphaForAll(GridBaseAlpha);
+	CurveSet->UI_SetShowGridXYForAll(bShowGridXY);
+	CurveSet->UI_SetShowGridXZForAll(bShowGridXZ);
+	CurveSet->UI_SetShowGridYZForAll(bShowGridYZ);
 	CurveSet->UI_SetLockToLocalXYForAll(bLockToLocalXY);
-	CurveSet->UI_SetForcePlanarForAll(bForcePlanar);
+	if (bForcePlanar)
+	{
+		CurveSet->UI_SetForcePlanarAxisForAll(ForcePlanarAxis);
+	}
+	else
+	{
+		CurveSet->UI_SetForcePlanarAxisForAll(EBezierPlanarAxis::None);
+	}
 	CurveSet->UI_SetEditInteractionEnabledForAll(bEnableEditMode, bShowControlPoints, bShowStrip);
 }
 
@@ -109,14 +126,31 @@ void ABezierDebugActor::ApplyCurveActorDebug() const
 		It->UI_SetShowControlPoints(bShowControlPoints);
 		It->UI_SetShowStrip(bShowStrip);
 		It->UI_SetShowCubeStrip(bUseCubeStrip);
-		It->UI_SetControlPointSize(ControlPointSize);
-		It->UI_SetStripSize(StripWidth, StripThickness);
+		if (bOverrideVisualSizes)
+		{
+			It->UI_SetControlPointSize(ControlPointSize);
+			It->UI_SetStripSize(StripWidth, StripThickness);
+		}
 		It->UI_SetControlPointColors(ControlPointNormal, ControlPointHover, ControlPointSelected);
 		It->UI_SetSnapToGrid(bSnapToGrid);
 		It->UI_SetShowGrid(bShowGrid);
 		It->UI_SetGridSizeCm(GridSizeCm);
+		It->UI_SetGridExtentCm(GridExtentCm);
+		It->UI_SetGridOriginWorld(GridOriginWorld);
+		It->UI_SetGridColor(GridColor);
+		It->UI_SetGridBaseAlpha(GridBaseAlpha);
+		It->UI_SetShowGridXY(bShowGridXY);
+		It->UI_SetShowGridXZ(bShowGridXZ);
+		It->UI_SetShowGridYZ(bShowGridYZ);
 		It->UI_SetLockToLocalXY(bLockToLocalXY);
-		It->UI_SetForcePlanar(bForcePlanar);
+		if (bForcePlanar)
+		{
+			It->UI_SetForcePlanarAxis(ForcePlanarAxis);
+		}
+		else
+		{
+			It->UI_SetForcePlanarAxis(EBezierPlanarAxis::None);
+		}
 		It->bShowControlPolygon = bShowControlPolygon;
 		It->bPulseDebugLines = bPulseDebugLines;
 		It->DebugPulseMinAlpha = DebugPulseMinAlpha;
@@ -125,13 +159,25 @@ void ABezierDebugActor::ApplyCurveActorDebug() const
 		It->bPulseControlPoints = bPulseControlPoints;
 		It->ControlPointPulseMinScale = ControlPointPulseMinScale;
 		It->ControlPointPulseMaxScale = ControlPointPulseMaxScale;
+		It->ControlPointPulseMinAlpha = ControlPointPulseMinAlpha;
+		It->ControlPointPulseMaxAlpha = ControlPointPulseMaxAlpha;
 		It->ControlPointPulseSpeed = ControlPointPulseSpeed;
 		It->bPulseStrip = bPulseStrip;
 		It->StripPulseMinWidth = StripPulseMinWidth;
 		It->StripPulseMaxWidth = StripPulseMaxWidth;
 		It->StripPulseMinThickness = StripPulseMinThickness;
 		It->StripPulseMaxThickness = StripPulseMaxThickness;
+		It->StripPulseMinAlpha = StripPulseMinAlpha;
+		It->StripPulseMaxAlpha = StripPulseMaxAlpha;
 		It->StripPulseSpeed = StripPulseSpeed;
+		It->bPulseGrid = bPulseGrid;
+		It->GridPulseMinAlpha = GridPulseMinAlpha;
+		It->GridPulseMaxAlpha = GridPulseMaxAlpha;
+		It->GridPulseSpeed = GridPulseSpeed;
+		It->GridPulseMinThickness = GridPulseMinThickness;
+		It->GridPulseMaxThickness = GridPulseMaxThickness;
+		It->bEnableVisualFade = bEnableVisualFade;
+		It->VisualFadeSpeed = VisualFadeSpeed;
 	}
 
 	for (TActorIterator<ABezierCurve2DActor> It(GetWorld()); It; ++It)
@@ -140,12 +186,20 @@ void ABezierDebugActor::ApplyCurveActorDebug() const
 		It->UI_SetShowControlPoints(bShowControlPoints);
 		It->UI_SetShowStrip(bShowStrip);
 		It->UI_SetShowCubeStrip(bUseCubeStrip);
-		It->UI_SetControlPointSize(ControlPointSize);
-		It->UI_SetStripSize(StripWidth, StripThickness);
+		if (bOverrideVisualSizes)
+		{
+			It->UI_SetControlPointSize(ControlPointSize);
+			It->UI_SetStripSize(StripWidth, StripThickness);
+		}
 		It->UI_SetControlPointColors(ControlPointNormal, ControlPointHover, ControlPointSelected);
 		It->UI_SetSnapToGrid(bSnapToGrid);
 		It->UI_SetShowGrid(bShowGrid);
 		It->UI_SetGridSizeCm(GridSizeCm);
+		It->UI_SetGridExtentCm(GridExtentCm);
+		It->UI_SetGridOriginWorld(GridOriginWorld);
+		It->UI_SetGridColor(GridColor);
+		It->UI_SetGridBaseAlpha(GridBaseAlpha);
+		It->UI_SetShowGridXY(bShowGridXY);
 		It->UI_SetLockToLocalXY(bLockToLocalXY);
 		It->UI_SetForcePlanar(bForcePlanar);
 		It->bShowControlPolygon = bShowControlPolygon;
@@ -156,13 +210,25 @@ void ABezierDebugActor::ApplyCurveActorDebug() const
 		It->bPulseControlPoints = bPulseControlPoints;
 		It->ControlPointPulseMinScale = ControlPointPulseMinScale;
 		It->ControlPointPulseMaxScale = ControlPointPulseMaxScale;
+		It->ControlPointPulseMinAlpha = ControlPointPulseMinAlpha;
+		It->ControlPointPulseMaxAlpha = ControlPointPulseMaxAlpha;
 		It->ControlPointPulseSpeed = ControlPointPulseSpeed;
 		It->bPulseStrip = bPulseStrip;
 		It->StripPulseMinWidth = StripPulseMinWidth;
 		It->StripPulseMaxWidth = StripPulseMaxWidth;
 		It->StripPulseMinThickness = StripPulseMinThickness;
 		It->StripPulseMaxThickness = StripPulseMaxThickness;
+		It->StripPulseMinAlpha = StripPulseMinAlpha;
+		It->StripPulseMaxAlpha = StripPulseMaxAlpha;
 		It->StripPulseSpeed = StripPulseSpeed;
+		It->bPulseGrid = bPulseGrid;
+		It->GridPulseMinAlpha = GridPulseMinAlpha;
+		It->GridPulseMaxAlpha = GridPulseMaxAlpha;
+		It->GridPulseSpeed = GridPulseSpeed;
+		It->GridPulseMinThickness = GridPulseMinThickness;
+		It->GridPulseMaxThickness = GridPulseMaxThickness;
+		It->bEnableVisualFade = bEnableVisualFade;
+		It->VisualFadeSpeed = VisualFadeSpeed;
 	}
 }
 
--- THREATEXEC/Public/BezierRuntimeTypes.h
+++ THREATEXEC/Public/BezierRuntimeTypes.h
@@ -9,3 +9,12 @@ enum class EBezierSamplingMode : uint8
 	Parametric UMETA(DisplayName = "Parametric"),
 	ArcLength  UMETA(DisplayName = "Arc Length")
 };
+
+UENUM(BlueprintType)
+enum class EBezierPlanarAxis : uint8
+{
+	None UMETA(DisplayName = "None"),
+	XY   UMETA(DisplayName = "XY (Z = 0)"),
+	XZ   UMETA(DisplayName = "XZ (Y = 0)"),
+	YZ   UMETA(DisplayName = "YZ (X = 0)")
+};
