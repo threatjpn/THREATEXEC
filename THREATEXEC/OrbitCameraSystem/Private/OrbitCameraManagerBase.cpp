7c4163a9dc658052980d5921937aa6512ebdc8dc
Zakir Khan <39201626+threatjpn@users.noreply.github.com>
2026-03-27 22:10:06 +0000

    Add orbit mouse controls (RMB look, MMB pan, wheel zoom) to manager

--- THREATEXEC/OrbitCameraSystem/Private/OrbitCameraManagerBase.cpp
+++ THREATEXEC/OrbitCameraSystem/Private/OrbitCameraManagerBase.cpp
@@ -1,20 +1,79 @@
 // Copyright 2020 RealVisStudios. All Rights Reserved.
 
 #include "OrbitCameraManagerBase.h"
+#include "Camera/CameraComponent.h"
+#include "CineCameraComponent.h"
+#include "Components/BoxComponent.h"
+#include "Components/InputComponent.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AOrbitCameraManagerBase::AOrbitCameraManagerBase()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void AOrbitCameraManagerBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (!ActiveOrbitCamera && bAutoFindOrbitCameraIfUnset)
+	{
+		TArray<AActor*> FoundOrbitCameras;
+		UGameplayStatics::GetAllActorsOfClass(this, AOrbitCameraBase::StaticClass(), FoundOrbitCameras);
+
+		AOrbitCameraBase* FirstOrbitCamera = nullptr;
+		AOrbitCameraBase* TaggedOrbitCamera = nullptr;
+		for (AActor* FoundActor : FoundOrbitCameras)
+		{
+			AOrbitCameraBase* FoundOrbitCamera = Cast<AOrbitCameraBase>(FoundActor);
+			if (!FoundOrbitCamera)
+			{
+				continue;
+			}
+
+			if (!FirstOrbitCamera)
+			{
+				FirstOrbitCamera = FoundOrbitCamera;
+			}
+
+			if (!AutoFindOrbitCameraTag.IsNone() && FoundOrbitCamera->ActorHasTag(AutoFindOrbitCameraTag))
+			{
+				TaggedOrbitCamera = FoundOrbitCamera;
+				break;
+			}
+		}
+
+		ActiveOrbitCamera = TaggedOrbitCamera ? TaggedOrbitCamera : FirstOrbitCamera;
+	}
+
+	if (bAutoPossessPlayer0OnBeginPlay)
+	{
+		if (APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0))
+		{
+			if (PlayerController->GetPawn() != this)
+			{
+				PlayerController->Possess(this);
+			}
+		}
+	}
+
+	if (ActiveOrbitCamera)
+	{
+		SetActorLocationAndRotation(ActiveOrbitCamera->GetActorLocation(), ActiveOrbitCamera->GetActorRotation());
+		CaptureOrbitCameraTransform(CurrentCameraDefinition, ActiveOrbitCamera);
+	}
+
+	if (bWalkModeOnly || bStartInWalkOutMode)
+	{
+		EnterWalkOutMode();
+	}
+
+	ApplyDesiredViewTarget();
 }
 
 // Called every frame
@@ -22,6 +81,12 @@ void AOrbitCameraManagerBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	UpdateModeTransition(DeltaTime);
+
+	if (bIsWalkOutMode)
+	{
+		UpdateWalkOutMovement(DeltaTime);
+	}
 }
 
 // Called to bind functionality to input
@@ -29,5 +94,402 @@ void AOrbitCameraManagerBase::SetupPlayerInputComponent(UInputComponent* PlayerI
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	if (!PlayerInputComponent)
+	{
+		return;
+	}
+
+	PlayerInputComponent->BindKey(ToggleWalkOutKey, IE_Pressed, this, &AOrbitCameraManagerBase::ToggleWalkOutMode);
+
+	PlayerInputComponent->BindKey(MoveForwardKey, IE_Pressed, this, &AOrbitCameraManagerBase::SetMoveForwardPressed);
+	PlayerInputComponent->BindKey(MoveForwardKey, IE_Released, this, &AOrbitCameraManagerBase::SetMoveForwardReleased);
+	PlayerInputComponent->BindKey(MoveBackwardKey, IE_Pressed, this, &AOrbitCameraManagerBase::SetMoveBackwardPressed);
+	PlayerInputComponent->BindKey(MoveBackwardKey, IE_Released, this, &AOrbitCameraManagerBase::SetMoveBackwardReleased);
+	PlayerInputComponent->BindKey(MoveRightKey, IE_Pressed, this, &AOrbitCameraManagerBase::SetMoveRightPressed);
+	PlayerInputComponent->BindKey(MoveRightKey, IE_Released, this, &AOrbitCameraManagerBase::SetMoveRightReleased);
+	PlayerInputComponent->BindKey(MoveLeftKey, IE_Pressed, this, &AOrbitCameraManagerBase::SetMoveLeftPressed);
+	PlayerInputComponent->BindKey(MoveLeftKey, IE_Released, this, &AOrbitCameraManagerBase::SetMoveLeftReleased);
+	PlayerInputComponent->BindKey(MoveUpKey, IE_Pressed, this, &AOrbitCameraManagerBase::SetMoveUpPressed);
+	PlayerInputComponent->BindKey(MoveUpKey, IE_Released, this, &AOrbitCameraManagerBase::SetMoveUpReleased);
+	PlayerInputComponent->BindKey(MoveDownKey, IE_Pressed, this, &AOrbitCameraManagerBase::SetMoveDownPressed);
+	PlayerInputComponent->BindKey(MoveDownKey, IE_Released, this, &AOrbitCameraManagerBase::SetMoveDownReleased);
+	PlayerInputComponent->BindKey(SprintKey, IE_Pressed, this, &AOrbitCameraManagerBase::SetSprintPressed);
+	PlayerInputComponent->BindKey(SprintKey, IE_Released, this, &AOrbitCameraManagerBase::SetSprintReleased);
+	PlayerInputComponent->BindKey(OrbitLookHoldKey, IE_Pressed, this, &AOrbitCameraManagerBase::SetOrbitLookPressed);
+	PlayerInputComponent->BindKey(OrbitLookHoldKey, IE_Released, this, &AOrbitCameraManagerBase::SetOrbitLookReleased);
+	PlayerInputComponent->BindKey(OrbitPanHoldKey, IE_Pressed, this, &AOrbitCameraManagerBase::SetOrbitPanPressed);
+	PlayerInputComponent->BindKey(OrbitPanHoldKey, IE_Released, this, &AOrbitCameraManagerBase::SetOrbitPanReleased);
+
+	PlayerInputComponent->BindAxisKey(EKeys::MouseX, this, &AOrbitCameraManagerBase::OnLookYaw);
+	PlayerInputComponent->BindAxisKey(EKeys::MouseY, this, &AOrbitCameraManagerBase::OnLookPitch);
+	PlayerInputComponent->BindAxisKey(EKeys::MouseWheelAxis, this, &AOrbitCameraManagerBase::OnOrbitMouseWheel);
 }
 
+void AOrbitCameraManagerBase::ToggleWalkOutMode()
+{
+	if (bWalkModeOnly)
+	{
+		EnterWalkOutMode();
+		return;
+	}
+
+	if (!bAllowWalkOutMode)
+	{
+		return;
+	}
+
+	if (bIsWalkOutMode)
+	{
+		ExitWalkOutMode();
+		return;
+	}
+
+	EnterWalkOutMode();
+}
+
+void AOrbitCameraManagerBase::EnterWalkOutMode()
+{
+	if (bIsWalkOutMode)
+	{
+		return;
+	}
+
+	bIsWalkOutMode = true;
+	WalkModeStartLocation = GetActorLocation();
+	WalkModeStartRotation = GetActorRotation();
+	WalkBoundsOrigin = WalkModeStartLocation;
+
+	if (ActiveOrbitCamera)
+	{
+		CaptureOrbitCameraTransform(StartCameraDefinition, ActiveOrbitCamera);
+	}
+
+	ApplyDesiredViewTarget();
+}
+
+void AOrbitCameraManagerBase::ExitWalkOutMode()
+{
+	if (bWalkModeOnly)
+	{
+		return;
+	}
+
+	if (!bIsWalkOutMode)
+	{
+		return;
+	}
+
+	bIsWalkOutMode = false;
+	bMoveForwardPressed = false;
+	bMoveBackwardPressed = false;
+	bMoveRightPressed = false;
+	bMoveLeftPressed = false;
+	bMoveUpPressed = false;
+	bMoveDownPressed = false;
+	bSprintPressed = false;
+
+	if (ActiveOrbitCamera)
+	{
+		TransitionToOrbitCamera(ActiveOrbitCamera, ModeTransitionDuration);
+	}
+
+	ApplyDesiredViewTarget();
+}
+
+void AOrbitCameraManagerBase::SetWalkOutModeEnabled(bool bEnable)
+{
+	if (bEnable)
+	{
+		EnterWalkOutMode();
+		return;
+	}
+
+	ExitWalkOutMode();
+}
+
+void AOrbitCameraManagerBase::ApplyDesiredViewTarget()
+{
+	if (!bAutoManageViewTarget)
+	{
+		return;
+	}
+
+	APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+	if (!PlayerController)
+	{
+		return;
+	}
+
+	AActor* DesiredViewTarget = bIsWalkOutMode ? Cast<AActor>(this) : Cast<AActor>(ActiveOrbitCamera.Get());
+	if (!DesiredViewTarget)
+	{
+		DesiredViewTarget = this;
+	}
+
+	if (PlayerController->GetViewTarget() != DesiredViewTarget)
+	{
+		PlayerController->SetViewTarget(DesiredViewTarget);
+	}
+}
+
+void AOrbitCameraManagerBase::CutToOrbitCamera(AOrbitCameraBase* NewOrbitCamera)
+{
+	if (!NewOrbitCamera)
+	{
+		return;
+	}
+
+	ActiveOrbitCamera = NewOrbitCamera;
+	bTransitionInProgress = false;
+	SetActorLocationAndRotation(NewOrbitCamera->GetActorLocation(), NewOrbitCamera->GetActorRotation());
+	CaptureOrbitCameraTransform(CurrentCameraDefinition, ActiveOrbitCamera);
+	ApplyDesiredViewTarget();
+}
+
+void AOrbitCameraManagerBase::TransitionToOrbitCamera(AOrbitCameraBase* NewOrbitCamera, float Duration)
+{
+	if (!NewOrbitCamera)
+	{
+		return;
+	}
+
+	ActiveOrbitCamera = NewOrbitCamera;
+	CaptureOrbitCameraTransform(TargetCameraDefinition, ActiveOrbitCamera);
+
+	TransitionStart = GetActorTransform();
+	TransitionTarget = ActiveOrbitCamera->GetActorTransform();
+	TransitionDuration = FMath::Max(0.0f, Duration);
+	TransitionElapsed = 0.0f;
+	bTransitionInProgress = TransitionDuration > 0.0f;
+
+	if (!bTransitionInProgress)
+	{
+		SetActorTransform(TransitionTarget);
+		CaptureOrbitCameraTransform(CurrentCameraDefinition, ActiveOrbitCamera);
+		ApplyDesiredViewTarget();
+	}
+}
+
+void AOrbitCameraManagerBase::UpdateWalkOutMovement(float DeltaTime)
+{
+	const FVector Forward = GetActorForwardVector();
+	const FVector Right = GetActorRightVector();
+	const FVector Up = FVector::UpVector;
+
+	float ForwardInput = 0.0f;
+	ForwardInput += bMoveForwardPressed ? 1.0f : 0.0f;
+	ForwardInput -= bMoveBackwardPressed ? 1.0f : 0.0f;
+
+	float RightInput = 0.0f;
+	RightInput += bMoveRightPressed ? 1.0f : 0.0f;
+	RightInput -= bMoveLeftPressed ? 1.0f : 0.0f;
+
+	float UpInput = 0.0f;
+	UpInput += bMoveUpPressed ? 1.0f : 0.0f;
+	UpInput -= bMoveDownPressed ? 1.0f : 0.0f;
+
+	FVector MoveDir = (Forward * ForwardInput) + (Right * RightInput) + (Up * UpInput);
+	if (MoveDir.IsNearlyZero())
+	{
+		return;
+	}
+
+	MoveDir = MoveDir.GetSafeNormal();
+	const float Speed = WalkMoveSpeed * (bSprintPressed ? WalkSprintMultiplier : 1.0f);
+
+	FVector Target = GetActorLocation() + (MoveDir * Speed * DeltaTime);
+	ClampWalkLocation(Target);
+	SetActorLocation(Target);
+}
+
+void AOrbitCameraManagerBase::UpdateModeTransition(float DeltaTime)
+{
+	if (!bTransitionInProgress)
+	{
+		return;
+	}
+
+	TransitionElapsed += DeltaTime;
+	const float Alpha = FMath::Clamp(TransitionElapsed / TransitionDuration, 0.0f, 1.0f);
+	const float SmoothedAlpha = FMath::SmoothStep(0.0f, 1.0f, Alpha);
+
+	const FVector Pos = FMath::Lerp(TransitionStart.GetLocation(), TransitionTarget.GetLocation(), SmoothedAlpha);
+	const FQuat Rot = FQuat::Slerp(TransitionStart.GetRotation(), TransitionTarget.GetRotation(), SmoothedAlpha);
+	SetActorLocationAndRotation(Pos, Rot);
+
+	if (Alpha >= 1.0f)
+	{
+		bTransitionInProgress = false;
+		if (ActiveOrbitCamera)
+		{
+			CaptureOrbitCameraTransform(CurrentCameraDefinition, ActiveOrbitCamera);
+		}
+	}
+}
+
+void AOrbitCameraManagerBase::ClampWalkLocation(FVector& InOutLocation) const
+{
+	if (!bEnableWalkBounds)
+	{
+		return;
+	}
+
+	if (WalkBoundsActor)
+	{
+		const UBoxComponent* Bounds = WalkBoundsActor->FindComponentByClass<UBoxComponent>();
+		if (Bounds)
+		{
+			const FVector Center = Bounds->GetComponentLocation();
+			const FVector Extent = Bounds->GetScaledBoxExtent();
+			InOutLocation.X = FMath::Clamp(InOutLocation.X, Center.X - Extent.X, Center.X + Extent.X);
+			InOutLocation.Y = FMath::Clamp(InOutLocation.Y, Center.Y - Extent.Y, Center.Y + Extent.Y);
+			InOutLocation.Z = FMath::Clamp(InOutLocation.Z, Center.Z - Extent.Z, Center.Z + Extent.Z);
+			return;
+		}
+	}
+
+	InOutLocation.X = FMath::Clamp(InOutLocation.X, WalkBoundsOrigin.X - WalkBoundsExtent.X, WalkBoundsOrigin.X + WalkBoundsExtent.X);
+	InOutLocation.Y = FMath::Clamp(InOutLocation.Y, WalkBoundsOrigin.Y - WalkBoundsExtent.Y, WalkBoundsOrigin.Y + WalkBoundsExtent.Y);
+	InOutLocation.Z = FMath::Clamp(InOutLocation.Z, WalkBoundsOrigin.Z - WalkBoundsExtent.Z, WalkBoundsOrigin.Z + WalkBoundsExtent.Z);
+}
+
+void AOrbitCameraManagerBase::CaptureOrbitCameraTransform(FOrbitCameraDefinition& OutDefinition, AOrbitCameraBase* InCamera) const
+{
+	if (!InCamera)
+	{
+		return;
+	}
+
+	OutDefinition.ActorTransform = InCamera->GetActorTransform();
+	OutDefinition.Position = InCamera->GetActorLocation();
+	OutDefinition.Rotation = InCamera->GetActorRotation();
+	OutDefinition.Distance = InCamera->Internal_CurrentDistance;
+	OutDefinition.Zoom = InCamera->Internal_CurrentFocalLength;
+	OutDefinition.FoV = InCamera->GetCineCameraComponent() ? InCamera->GetCineCameraComponent()->FieldOfView : OutDefinition.FoV;
+}
+
+void AOrbitCameraManagerBase::SetMoveForwardPressed() { bMoveForwardPressed = true; }
+void AOrbitCameraManagerBase::SetMoveForwardReleased() { bMoveForwardPressed = false; }
+void AOrbitCameraManagerBase::SetMoveBackwardPressed() { bMoveBackwardPressed = true; }
+void AOrbitCameraManagerBase::SetMoveBackwardReleased() { bMoveBackwardPressed = false; }
+void AOrbitCameraManagerBase::SetMoveRightPressed() { bMoveRightPressed = true; }
+void AOrbitCameraManagerBase::SetMoveRightReleased() { bMoveRightPressed = false; }
+void AOrbitCameraManagerBase::SetMoveLeftPressed() { bMoveLeftPressed = true; }
+void AOrbitCameraManagerBase::SetMoveLeftReleased() { bMoveLeftPressed = false; }
+void AOrbitCameraManagerBase::SetMoveUpPressed() { bMoveUpPressed = true; }
+void AOrbitCameraManagerBase::SetMoveUpReleased() { bMoveUpPressed = false; }
+void AOrbitCameraManagerBase::SetMoveDownPressed() { bMoveDownPressed = true; }
+void AOrbitCameraManagerBase::SetMoveDownReleased() { bMoveDownPressed = false; }
+void AOrbitCameraManagerBase::SetSprintPressed() { bSprintPressed = true; }
+void AOrbitCameraManagerBase::SetSprintReleased() { bSprintPressed = false; }
+void AOrbitCameraManagerBase::SetOrbitLookPressed() { bOrbitLookPressed = true; }
+void AOrbitCameraManagerBase::SetOrbitLookReleased() { bOrbitLookPressed = false; }
+void AOrbitCameraManagerBase::SetOrbitPanPressed() { bOrbitPanPressed = true; }
+void AOrbitCameraManagerBase::SetOrbitPanReleased() { bOrbitPanPressed = false; }
+
+void AOrbitCameraManagerBase::OnOrbitMouseWheel(float Value)
+{
+	if (bIsWalkOutMode || FMath::IsNearlyZero(Value) || !ActiveOrbitCamera || !ActiveOrbitCamera->CineCamRef)
+	{
+		return;
+	}
+
+	const float DistanceMin = FMath::Min(ActiveOrbitCamera->MinDistance, ActiveOrbitCamera->MaxDistance);
+	const float DistanceMax = FMath::Max(ActiveOrbitCamera->MinDistance, ActiveOrbitCamera->MaxDistance);
+	const float DistanceNow = FMath::Abs(ActiveOrbitCamera->CineCamRef->GetRelativeLocation().X);
+	const float DistanceTarget = FMath::Clamp(DistanceNow - (Value * OrbitZoomStep), DistanceMin, DistanceMax);
+	ActiveOrbitCamera->CineCamRef->SetRelativeLocation(FVector(-DistanceTarget, 0.0f, 0.0f));
+	ActiveOrbitCamera->Internal_CurrentDistance = DistanceTarget;
+	ActiveOrbitCamera->Internal_TargetDistance = DistanceTarget;
+
+	const float FocalMin = FMath::Min(ActiveOrbitCamera->MinFocalLength, ActiveOrbitCamera->MaxFocalLength);
+	const float FocalMax = FMath::Max(ActiveOrbitCamera->MinFocalLength, ActiveOrbitCamera->MaxFocalLength);
+	const float FocalTarget = FMath::Clamp(ActiveOrbitCamera->CineCamRef->CurrentFocalLength + (Value * 1.5f), FocalMin, FocalMax);
+	ActiveOrbitCamera->CineCamRef->CurrentFocalLength = FocalTarget;
+	ActiveOrbitCamera->Internal_CurrentFocalLength = FocalTarget;
+	ActiveOrbitCamera->Internal_TargetFocalLength = FocalTarget;
+}
+
+void AOrbitCameraManagerBase::OnLookYaw(float Value)
+{
+	if (FMath::IsNearlyZero(Value))
+	{
+		return;
+	}
+
+	const float DeltaSeconds = GetWorld() ? GetWorld()->GetDeltaSeconds() : 0.0f;
+	if (bIsWalkOutMode)
+	{
+		FRotator Rot = GetActorRotation();
+		Rot.Yaw += Value * WalkLookYawSpeed * DeltaSeconds;
+		SetActorRotation(Rot);
+		return;
+	}
+
+	if (!ActiveOrbitCamera || !ActiveOrbitCamera->OrbitRoot)
+	{
+		return;
+	}
+
+	if (bOrbitPanPressed)
+	{
+		const FVector RightMove = ActiveOrbitCamera->OrbitRoot->GetRightVector() * (Value * OrbitPanSpeed * 25.0f);
+		ActiveOrbitCamera->OrbitRoot->AddWorldOffset(RightMove);
+		return;
+	}
+
+	if (!bOrbitLookPressed)
+	{
+		return;
+	}
+
+	FRotator OrbitRot = ActiveOrbitCamera->OrbitRoot->GetRelativeRotation();
+	const float YawMin = FMath::Min(ActiveOrbitCamera->MinYaw, ActiveOrbitCamera->MaxYaw);
+	const float YawMax = FMath::Max(ActiveOrbitCamera->MinYaw, ActiveOrbitCamera->MaxYaw);
+	OrbitRot.Yaw = FMath::Clamp(OrbitRot.Yaw + (Value * OrbitLookYawSpeed * DeltaSeconds), YawMin, YawMax);
+	ActiveOrbitCamera->OrbitRoot->SetRelativeRotation(OrbitRot);
+	ActiveOrbitCamera->Internal_CurrentRotation = OrbitRot;
+	ActiveOrbitCamera->Internal_TargetRotation = OrbitRot;
+}
+
+void AOrbitCameraManagerBase::OnLookPitch(float Value)
+{
+	if (FMath::IsNearlyZero(Value))
+	{
+		return;
+	}
+
+	const float DeltaSeconds = GetWorld() ? GetWorld()->GetDeltaSeconds() : 0.0f;
+	if (bIsWalkOutMode)
+	{
+		FRotator Rot = GetActorRotation();
+		Rot.Pitch = FMath::Clamp(Rot.Pitch + (-Value * WalkLookPitchSpeed * DeltaSeconds), WalkMinPitch, WalkMaxPitch);
+		SetActorRotation(Rot);
+		return;
+	}
+
+	if (!ActiveOrbitCamera || !ActiveOrbitCamera->OrbitRoot)
+	{
+		return;
+	}
+
+	if (bOrbitPanPressed)
+	{
+		const FVector UpMove = ActiveOrbitCamera->OrbitRoot->GetUpVector() * (-Value * OrbitPanSpeed * 25.0f);
+		ActiveOrbitCamera->OrbitRoot->AddWorldOffset(UpMove);
+		return;
+	}
+
+	if (!bOrbitLookPressed)
+	{
+		return;
+	}
+
+	FRotator OrbitRot = ActiveOrbitCamera->OrbitRoot->GetRelativeRotation();
+	const float PitchMin = FMath::Min(ActiveOrbitCamera->MinPitch, ActiveOrbitCamera->MaxPitch);
+	const float PitchMax = FMath::Max(ActiveOrbitCamera->MinPitch, ActiveOrbitCamera->MaxPitch);
+	OrbitRot.Pitch = FMath::Clamp(OrbitRot.Pitch + (-Value * OrbitLookPitchSpeed * DeltaSeconds), PitchMin, PitchMax);
+	ActiveOrbitCamera->OrbitRoot->SetRelativeRotation(OrbitRot);
+	ActiveOrbitCamera->Internal_CurrentRotation = OrbitRot;
+	ActiveOrbitCamera->Internal_TargetRotation = OrbitRot;
+}
--- THREATEXEC/OrbitCameraSystem/Public/OrbitCameraManagerBase.h
+++ THREATEXEC/OrbitCameraSystem/Public/OrbitCameraManagerBase.h
@@ -8,6 +8,7 @@
 // Unreal Includes
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputCoreTypes.h"
 
 #include "OrbitCameraManagerBase.generated.h"
 
@@ -78,6 +79,168 @@ public:
 
 #pragma endregion
 
+#pragma region OrbitAndWalkMode
+
+	// Enables standalone walk mode usage with no orbit camera dependency.
+	// When enabled the pawn starts in walk mode and does not transition back to orbit.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Mode")
+	bool bWalkModeOnly = false;
+
+	// If true, starts the pawn in walk out mode on BeginPlay.
+	// Ignored when bWalkModeOnly is enabled because walk mode is forced on.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Mode")
+	bool bStartInWalkOutMode = false;
+
+	// Camera that remains the source orbit camera when switching to walk out.
+	// Edit this on placed level instances (not class defaults) so actor picking works as expected.
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "OrbitCamera|Mode")
+	TObjectPtr<AOrbitCameraBase> ActiveOrbitCamera = nullptr;
+
+	// If true and ActiveOrbitCamera is unset, manager will auto-bind to the first matching OrbitCameraBase in the level.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Mode")
+	bool bAutoFindOrbitCameraIfUnset = true;
+
+	// Optional tag filter for auto-discovery. If set, manager prefers cameras with this tag.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Mode",
+		meta = (EditCondition = "bAutoFindOrbitCameraIfUnset"))
+	FName AutoFindOrbitCameraTag = NAME_None;
+
+	// If true, manager will possess itself with PlayerController(0) on BeginPlay for plug-and-play setup.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Mode")
+	bool bAutoPossessPlayer0OnBeginPlay = true;
+
+	// If true, manager updates player view target automatically when entering/exiting walk mode.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Mode")
+	bool bAutoManageViewTarget = true;
+
+	// True while player is in walk out mode.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "OrbitCamera|Mode")
+	bool bIsWalkOutMode = false;
+
+	// When true, player can toggle between orbit and walk out mode.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Mode")
+	bool bAllowWalkOutMode = true;
+
+	// Duration used for mode transition blending.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Mode", meta = (ClampMin = "0.0", UIMin = "0.0"))
+	float ModeTransitionDuration = 0.35f;
+
+	// Movement speed in walk out mode.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|WalkOut", meta = (ClampMin = "0.0", UIMin = "0.0"))
+	float WalkMoveSpeed = 350.0f;
+
+	// Fast movement speed while sprint input is active.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|WalkOut", meta = (ClampMin = "0.0", UIMin = "0.0"))
+	float WalkSprintMultiplier = 2.0f;
+
+	// Mouse/controller yaw speed multiplier.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|WalkOut", meta = (ClampMin = "0.0", UIMin = "0.0"))
+	float WalkLookYawSpeed = 120.0f;
+
+	// Mouse/controller pitch speed multiplier.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|WalkOut", meta = (ClampMin = "0.0", UIMin = "0.0"))
+	float WalkLookPitchSpeed = 120.0f;
+
+	// Orbit mouse look speed while holding orbit-look key.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|OrbitControls", meta = (ClampMin = "0.0", UIMin = "0.0"))
+	float OrbitLookYawSpeed = 100.0f;
+
+	// Orbit mouse pitch speed while holding orbit-look key.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|OrbitControls", meta = (ClampMin = "0.0", UIMin = "0.0"))
+	float OrbitLookPitchSpeed = 100.0f;
+
+	// Orbit pan speed while holding orbit-pan key.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|OrbitControls", meta = (ClampMin = "0.0", UIMin = "0.0"))
+	float OrbitPanSpeed = 0.6f;
+
+	// Orbit zoom speed per mouse wheel tick.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|OrbitControls", meta = (ClampMin = "0.0", UIMin = "0.0"))
+	float OrbitZoomStep = 25.0f;
+
+	// Clamp pitch while walking to keep controls predictable.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|WalkOut", meta = (ClampMin = "-89.0", ClampMax = "89.0"))
+	float WalkMinPitch = -80.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|WalkOut", meta = (ClampMin = "-89.0", ClampMax = "89.0"))
+	float WalkMaxPitch = 80.0f;
+
+	// Optional local bounds around walk origin.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|WalkOut|Bounds")
+	bool bEnableWalkBounds = false;
+
+	// Axis-aligned extents in centimeters around walk origin.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|WalkOut|Bounds",
+		meta = (ClampMin = "0.0", UIMin = "0.0", EditCondition = "bEnableWalkBounds"))
+	FVector WalkBoundsExtent = FVector(1000.0f, 1000.0f, 500.0f);
+
+	// Optional hard actor bounds for walk mode. Requires a BoxComponent on actor.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|WalkOut|Bounds",
+		meta = (EditCondition = "bEnableWalkBounds"))
+	AActor* WalkBoundsActor = nullptr;
+
+#pragma endregion
+
+#pragma region Keybinds
+
+	// Default keybinds are coded here so BP/UE input setup stays straightforward.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey ToggleWalkOutKey = EKeys::Tab;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey MoveForwardKey = EKeys::W;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey MoveBackwardKey = EKeys::S;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey MoveRightKey = EKeys::D;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey MoveLeftKey = EKeys::A;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey MoveUpKey = EKeys::E;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey MoveDownKey = EKeys::Q;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey SprintKey = EKeys::LeftShift;
+
+	// Hold to rotate orbit camera with mouse movement.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey OrbitLookHoldKey = EKeys::RightMouseButton;
+
+	// Hold to pan orbit origin with mouse movement.
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "OrbitCamera|Input|Keybinds")
+	FKey OrbitPanHoldKey = EKeys::MiddleMouseButton;
+
+#pragma endregion
+
+	// Switches between orbit and walk out mode.
+	UFUNCTION(BlueprintCallable, Category = "OrbitCamera|Mode")
+	void ToggleWalkOutMode();
+
+	// Enter walk out mode without changing ActiveOrbitCamera settings.
+	UFUNCTION(BlueprintCallable, Category = "OrbitCamera|Mode")
+	void EnterWalkOutMode();
+
+	// Return to orbit camera mode.
+	UFUNCTION(BlueprintCallable, Category = "OrbitCamera|Mode")
+	void ExitWalkOutMode();
+
+	// Explicitly set walk out mode state (supports walk-only levels without orbit cameras).
+	UFUNCTION(BlueprintCallable, Category = "OrbitCamera|Mode")
+	void SetWalkOutModeEnabled(bool bEnable);
+
+	// Assign active orbit camera and move manager to it.
+	UFUNCTION(BlueprintCallable, Category = "OrbitCamera|Transition")
+	void CutToOrbitCamera(AOrbitCameraBase* NewOrbitCamera);
+
+	// Transition manager transform to orbit camera over duration.
+	UFUNCTION(BlueprintCallable, Category = "OrbitCamera|Transition")
+	void TransitionToOrbitCamera(AOrbitCameraBase* NewOrbitCamera, float Duration = 0.5f);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -88,4 +251,54 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void ApplyDesiredViewTarget();
+	void UpdateWalkOutMovement(float DeltaTime);
+	void UpdateModeTransition(float DeltaTime);
+	void ClampWalkLocation(FVector& InOutLocation) const;
+	void CaptureOrbitCameraTransform(FOrbitCameraDefinition& OutDefinition, AOrbitCameraBase* InCamera) const;
+
+	void SetMoveForwardPressed();
+	void SetMoveForwardReleased();
+	void SetMoveBackwardPressed();
+	void SetMoveBackwardReleased();
+	void SetMoveRightPressed();
+	void SetMoveRightReleased();
+	void SetMoveLeftPressed();
+	void SetMoveLeftReleased();
+	void SetMoveUpPressed();
+	void SetMoveUpReleased();
+	void SetMoveDownPressed();
+	void SetMoveDownReleased();
+	void SetSprintPressed();
+	void SetSprintReleased();
+	void SetOrbitLookPressed();
+	void SetOrbitLookReleased();
+	void SetOrbitPanPressed();
+	void SetOrbitPanReleased();
+	void OnOrbitMouseWheel(float Value);
+
+	void OnLookYaw(float Value);
+	void OnLookPitch(float Value);
+
+	FVector WalkBoundsOrigin = FVector::ZeroVector;
+	FVector WalkModeStartLocation = FVector::ZeroVector;
+	FRotator WalkModeStartRotation = FRotator::ZeroRotator;
+
+	bool bMoveForwardPressed = false;
+	bool bMoveBackwardPressed = false;
+	bool bMoveRightPressed = false;
+	bool bMoveLeftPressed = false;
+	bool bMoveUpPressed = false;
+	bool bMoveDownPressed = false;
+	bool bSprintPressed = false;
+	bool bOrbitLookPressed = false;
+	bool bOrbitPanPressed = false;
+
+	bool bTransitionInProgress = false;
+	float TransitionElapsed = 0.0f;
+	float TransitionDuration = 0.0f;
+	FTransform TransitionStart = FTransform::Identity;
+	FTransform TransitionTarget = FTransform::Identity;
 };
