024d5efa7be8092bbd3753cbe43bc86863921a0d
Zakir Khan <39201626+threatjpn@users.noreply.github.com>
2026-04-06 17:10:07 +0100

    Expose preview text font and styling controls

--- THREATEXEC/Public/PhotoLocationNameWidget.h
+++ THREATEXEC/Public/PhotoLocationNameWidget.h
@@ -6,14 +6,33 @@
 
 class UTextBlock;
 
+USTRUCT(BlueprintType)
+struct FPhotoLocationDisplayNameOverride
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Photo Location")
+    FString LevelName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Photo Location")
+    FText DisplayName;
+};
+
 UCLASS()
 class THREATEXEC_API UPhotoLocationNameWidget : public UUserWidget
 {
     GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Photo Location")
+    TArray<FPhotoLocationDisplayNameOverride> LevelDisplayNameOverrides;
 
 protected:
     virtual void NativeConstruct() override;
 
     UPROPERTY(meta = (BindWidget))
     TObjectPtr<UTextBlock> TXT_LocationName;
+
+private:
+    FText ResolveDisplayNameForLevel(const FString& LevelName) const;
 };
